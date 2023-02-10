7b0fb8e6161dba1a135dafc3c049c29d107ae297
SanskarKashyap <sanskarkashyap308@gmail.com>
2023-02-10 10:37:52 +0530

    initial changes

---
+++ lib/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return Container(
+    //   color: Colors.purple,
+    //   child: Center(child: Text("Hellow Bachoo !")),
+    // );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text
+          ("CockTail App"),
+        ),
+      ),
+    );
+  }
+}
